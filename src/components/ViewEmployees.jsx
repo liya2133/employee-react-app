444331d7c209127749e87191ad2c2c19021c96b9
liya2133 <liyasebastian2108@gmail.com>
2026-07-10 12:01:17 +0530

    updated

--- src/components/ViewEmployees.jsx
+++ src/components/ViewEmployees.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const ViewEmployees = () => {
+
+const [data, changeData] = useState(
+  [
+    { "name": "liya sebastian","code":"001","number":"8848888901","designation":"Software Engineer" },
+    { "name": "krishna gadha ug","code":"002","number":"8848978902","designation":"web Developer" },
+     { "name": "angel wilson","code":"003","number":"8844568903","designation":"tester" },
+    { "name": "maria palatty","code":"004","number":"9717488904","designation":"HR" },
+     { "name": "vinaya biju","code":"005","number":"9921889005","designation":"Frontend Developer" },
+     { "name": "joyel joji","code":"006","number":"8848888900","designation":"Software Engineer" },
+    { "name": "alant saji","code":"007","number":"8848978989","designation":"web Developer" },
+     { "name": "charls thomas","code":"008","number":"8844568909","designation":"tester" },
+    { "name": "varsha johnson","code":"009","number":"9717488912","designation":"HR" },
+     { "name": "shaniya shaju","code":"010","number":"9921889045","designation":"Frontend Developer" },
+
+  ]
+)
+
+
   return (
     <div>
       <NavigationBar />
@@ -10,99 +28,34 @@ const ViewEmployees = () => {
         <h2 className="text-center mb-4">
           <b><i>VIEW ALL EMPLOYEES</i></b>
         </h2>
-
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
               <th scope="col">Employee Name</th>
               <th scope="col">Employee Code</th>
               <th scope="col">Phone Number</th>
               <th scope="col">Designation</th>
             </tr>
           </thead>
-
-          <tbody>
+          
+                <tbody>
+                  {data.map
+          (
+            (value,index)=>{
+              return(
             <tr>
-              <th scope="row">1</th>
-              <td>Ann Maria</td>
-              <td>EMP001</td>
-              <td>9876543210</td>
-              <td>Software Engineer</td>
+              <td>{value.name}</td>
+              <td>{value.code}</td>
+              <td>{value.number}</td>
+              <td>{value.designation}</td>
             </tr>
 
-            <tr>
-              <th scope="row">2</th>
-              <td>Anu Maria</td>
-              <td>EMP002</td>
-              <td>9876543211</td>
-              <td>HR Manager</td>
-            </tr>
-
-            <tr>
-              <th scope="row">3</th>
-              <td>Liya Sebastian</td>
-              <td>EMP003</td>
-              <td>9876543212</td>
-              <td>UI/UX Designer</td>
-            </tr>
-
-            <tr>
-              <th scope="row">4</th>
-              <td>Meera Joseph</td>
-              <td>EMP004</td>
-              <td>9876543213</td>
-              <td>Project Manager</td>
-            </tr>
-
-            <tr>
-              <th scope="row">5</th>
-              <td>Angel Wilson</td>
-              <td>EMP005</td>
-              <td>9876543214</td>
-              <td>System Analyst</td>
-            </tr>
-
-            <tr>
-              <th scope="row">6</th>
-              <td>Sneha Roy</td>
-              <td>EMP006</td>
-              <td>9876543215</td>
-              <td>QA Engineer</td>
-            </tr>
-
-            <tr>
-              <th scope="row">7</th>
-              <td>Alant Saji</td>
-              <td>EMP007</td>
-              <td>9876543216</td>
-              <td>Backend Developer</td>
-            </tr>
-
-            <tr>
-              <th scope="row">8</th>
-              <td>Neha Mathew</td>
-              <td>EMP008</td>
-              <td>9876543217</td>
-              <td>Frontend Developer</td>
-            </tr>
-
-            <tr>
-              <th scope="row">9</th>
-              <td>John Mathew</td>
-              <td>EMP009</td>
-              <td>9876543218</td>
-              <td>Network Engineer</td>
-            </tr>
-
-            <tr>
-              <th scope="row">10</th>
-              <td>Priya Menon</td>
-              <td>EMP010</td>
-              <td>9876543219</td>
-              <td>Database Administrator</td>
-            </tr>
+             )
+            }
+          )}
           </tbody>
+             
+          
         </table>
       </div>
     </div>
